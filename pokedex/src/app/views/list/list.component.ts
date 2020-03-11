67bb283928697f488656e8ee19b17926e499ca2d
Jonny Costa Alves <jcostaal@everis.com>
2020-03-11 11:32:37 -0300

    exibindo os pokemons na tela de forma estatica

--- pokedex/src/app/views/list/list.component.ts
+++ pokedex/src/app/views/list/list.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  //array que armazena os pokemons
+  pokemonList = [
+    { name: 'Bulbasaur', number: 1},
+    { name: 'Charmander', number: 4},
+    { name: 'Squirtle', number: 7},
+    { name: 'Pikachu', number: 25}
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
